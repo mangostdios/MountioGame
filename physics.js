55f9409dc27a359058a320848265a100f6fe36e1
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-01 15:25:19 -0500

    Create physics.js

---
+++ physics.js
@@ -0,0 +1,28 @@
+var yv;
+var xv;
+var x;
+var y;
+var physics = {
+friction: 0.7,
+gravity: 2,
+code: function() {
+yv += physics.gravity;
+y += yv;
+x += xv;
+xv = xv * physics.friction;
+//draw character at (x,y);
+if(/*collision*/) {
+//platformer script
+}
+},
+interval: setInterval(physics.code,10)
+};
+
+document.getElementsByTagName("body").addEventListener("onmousedown", function(event){
+if (event.key === a) {
+xv=-10;
+}
+if (event.key === d) {
+xv=10;
+}
+});
